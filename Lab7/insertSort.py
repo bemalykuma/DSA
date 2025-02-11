5a1207e1e90f63cb7f085ef4db5e306730f9437e
Kumashiro <139941786+bemalykuma@users.noreply.github.com>
2025-02-12 02:02:02 +0700

    Lab7

---
+++ Lab7/insertSort.py
@@ -0,0 +1,26 @@
+import json
+
+def insertSort(list, last):
+    c = 1
+    count_in, count_out = 0, 0
+    while last:
+        hold = c
+        walk = c - 1
+        while walk >= 0 and list[hold] < list[walk]:
+            list.insert(walk+1, list.pop(walk))
+            hold -= 1
+            walk -= 1
+            count_in += 1
+        if walk >= 0 and list[hold] > list[walk]:
+            count_out += 1
+        hold = walk + 1
+        c += 1
+        print(list)
+        last -= 1
+    print("Comparison times:", count_in + count_out)
+    return
+
+def main():
+    l = json.loads(input())
+    insertSort(l,int(input()))
+main()
